2966338a1f412d4dfbe28ba57c30a2f7c0728592
Martin Kamau <martinkimu09@gmail.com>
2022-08-28 22:28:37 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    addTask(event.target.new-task-description.value);
+    form.reset();
+  })
+
+//
+  function addTask(event) {
+  li = document.createElement('li');
+  li.innerText = event;
+
+  document.getElementById('tasks').appendChild('li');
+  li.remove();
+  }
+  let btn = document.createElement('button');
+  li.appendChild(btn);
+  removeTask(btn);
+//
+  function removeTask(event) {
+    event.addEventListener('click', () => e.parentNode.remove());
+  }
 });
